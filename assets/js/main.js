3065a30443d743e6a90e8e9a0184b247a0067c44
DESKTOP-MDBS3BI\badacho <choieeejeong@gmail.com>
2021-10-29 11:38:09 +0900

    드롭메뉴 스크립트 & 찾기 메뉴 제작 진행

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,10 @@
 $(document).ready(function () {
   const gnbBtn = $(".gnb-btn");
   const subMenu = $(".sub-menu");
+  const dropMenu = $(".drop-menu");
   const closeBtn = $(".close-btn");
+  const menuBtn = $(".menu-btn");
+  const dropCloseBtn = $(".drop-close-btn");
 
   gnbBtn.on({
     click: function (e) {
@@ -17,6 +20,20 @@ $(document).ready(function () {
       e.preventDefault();
       gnbBtn.removeClass("active");
       subMenu.slideUp(400);
+    },
+  });
+
+  menuBtn.on({
+    click: function (e) {
+      e.preventDefault();
+      dropMenu.slideToggle(300);
+    },
+  });
+
+  dropCloseBtn.on({
+    click: function (e) {
+      e.preventDefault();
+      dropMenu.slideUp(300);
     },
   });
 });
