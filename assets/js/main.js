aee32241f08c056d1e67af5d51e6628b697dca94
DESKTOP-MDBS3BI\badacho <choieeejeong@gmail.com>
2021-10-29 13:59:47 +0900

    검색 드롭 박스 제작

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,17 +2,27 @@ $(document).ready(function () {
   const gnbBtn = $(".gnb-btn");
   const subMenu = $(".sub-menu");
   const dropMenu = $(".drop-menu");
+  const dep2SearchBox = $(".dep2-search-box");
+
   const closeBtn = $(".close-btn");
   const menuBtn = $(".menu-btn");
+  const searchBtn = $(".search-drop-btn");
   const dropCloseBtn = $(".drop-close-btn");
+  const searchCloseBtn = $(".search-close-btn");
 
   gnbBtn.on({
     click: function (e) {
       e.preventDefault();
-      subMenu.hide();
-      gnbBtn.removeClass("active");
-      $(this).next().slideDown(300);
-      $(this).addClass("active");
+
+      if ($(this).hasClass("active") == true) {
+        subMenu.slideUp(300);
+        $(this).removeClass("active");
+      } else {
+        subMenu.hide();
+        gnbBtn.removeClass("active");
+        $(this).next().slideToggle(300);
+        $(this).addClass("active");
+      }
     },
   });
   closeBtn.on({
@@ -34,6 +44,19 @@ $(document).ready(function () {
     click: function (e) {
       e.preventDefault();
       dropMenu.slideUp(300);
+    },
+  });
+
+  searchBtn.on({
+    click: function (e) {
+      e.preventDefault();
+      dep2SearchBox.slideToggle(300);
+    },
+  });
+  searchCloseBtn.on({
+    click: function (e) {
+      e.preventDefault();
+      dep2SearchBox.slideUp(300);
     },
   });
 });
