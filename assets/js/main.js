af937b37554847a90558eba1200791aa722c6440
DESKTOP-MDBS3BI\badacho <choieeejeong@gmail.com>
2021-11-05 15:41:55 +0900

    모바일 공지사항 수정 & 메뉴 버튼 스크립트 수정

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,19 @@
 $(document).ready(function () {
   const $window = $(window);
-  const gnbBox = $(".gnb-box");
-  const headerRightBox = $(".header .right-box");
+  const navGnbBox = $(".gnb-box");
+  const searchBox = $(".right-box .search-box");
+  const mobilesearchBox = $(".mobile-search-box");
   const gnbBtn = $(".gnb-btn");
   const subMenu = $(".sub-menu");
   const dropMenu = $(".drop-menu");
   const dep2SearchBox = $(".dep2-search-box");
-  const mobileLnbMenu = $(".mobile-lnb-menu");
+  const mobileLnbMenu = $(".mobile-lnb-wrap");
   const mobileSearchMenu = $(".mobile-search-menu");
   const typeListBox = $(".type-list-box");
 
   const closeBtn = $(".close-btn");
-  const menuBtn = $(".search-box .menu-btn");
-  const searchBtn = $(".search-drop-btn");
-  const mobileSearchBtn = $(".mobile-search-btn");
   const mobileSearchCloseBtn = $(".mobile-search-close-btn");
-  const mobileMenuBtn = $(".mobile-menu-btn");
   const searchTypeBtn = $(".type-btn");
-
   const dropCloseBtn = $(".drop-close-btn");
   const searchCloseBtn = $(".search-close-btn");
   const mobileLnbCloseBtn = $(".mobile-lnb-close-btn");
@@ -44,47 +40,77 @@ $(document).ready(function () {
     },
   });
 
-  gnbBox.mouseleave(function () {
+  navGnbBox.mouseleave(function () {
     subMenu.hide();
     gnbBtn.removeClass("active");
   });
 
-  //웹 햄버거 버튼
+  //웹 햄버거 버튼 & 서치버튼
 
-  menuBtn.on({
+  searchBox.on({
     click: function (e) {
+      let target = e.target;
       e.preventDefault();
-      dropMenu.toggleClass("active");
+
+      if (target.matches(".menu-btn") || target.matches(".menu-btn > span")) {
+        dep2SearchBox.slideUp(300);
+        dropMenu.slideToggle(300);
+      } else if (target.matches(".search-drop-btn") || target.matches(".search-drop-btn > span")) {
+        dropMenu.slideUp(300);
+        dep2SearchBox.slideToggle(300);
+      }
     },
   });
+
+  dropMenu.on({
+    mouseleave: function () {
+      $(this).slideUp(300);
+    },
+  });
+
+  dep2SearchBox.on({
+    mouseleave: function (e) {
+      $(this).slideUp(300);
+    },
+  });
+
+  // 웹 햄버거 & 서치 닫기 버튼
 
   dropCloseBtn.on({
     click: function (e) {
       e.preventDefault();
-      dropMenu.removeClass("active");
+      dropMenu.slideUp(300);
     },
   });
 
-  //웹 서치 버튼
-
-  searchBtn.on({
-    click: function (e) {
-      e.preventDefault();
-      dep2SearchBox.toggleClass("active");
-    },
-  });
   searchCloseBtn.on({
     click: function (e) {
       e.preventDefault();
-      dep2SearchBox.removeClass("active");
+      dep2SearchBox.slideUp(300);
     },
   });
 
-  //모바일 테블릿 햄버거 버튼
-  mobileMenuBtn.on({
+  //모바일 테블릿 햄버거 & 서치 버튼
+  mobilesearchBox.on({
     click: function (e) {
+      let target = e.target;
       e.preventDefault();
-      mobileLnbMenu.addClass("mobile-show");
+
+      if (target.matches(".mobile-menu-btn") || target.matches(".mobile-menu-btn > span")) {
+        mobileSearchMenu.slideUp(300);
+        mobileLnbMenu.addClass("mobile-show");
+      } else if (target.matches(".mobile-search-btn")) {
+        mobileSearchMenu.slideToggle(300);
+      }
+    },
+  });
+
+  mobileLnbMenu.on({
+    click: function (e) {
+      let target = e.target;
+      if (target.matches(".mobile-lnb-wrap")) {
+        mobileLnbMenu.removeClass("mobile-show");
+      }
     },
   });
 
@@ -95,18 +121,16 @@ $(document).ready(function () {
     },
   });
 
-  //모바일 -테블릿 서치버튼
-  mobileSearchBtn.on({
-    click: function (e) {
-      e.preventDefault();
-      mobileSearchMenu.toggleClass("active");
-    },
-  });
-
   mobileSearchCloseBtn.on({
     click: function (e) {
       e.preventDefault();
-      mobileSearchMenu.removeClass("active");
+      mobileSearchMenu.slideUp(300);
+    },
+  });
+
+  mobileSearchMenu.on({
+    mouseleave: function () {
+      $(this).slideUp(300);
     },
   });
 
@@ -125,4 +149,22 @@ $(document).ready(function () {
       }
     },
   });
+
+  $window.resize(function () {
+    if ($window.innerWidth() > 768) {
+      mobileSubhide();
+    }
+    if ($window.innerWidth() <= 768) {
+      webSubhide();
+    }
+  });
+
+  function webSubhide() {
+    dropMenu.slideUp(300);
+    dep2SearchBox.slideUp(300);
+  }
+  function mobileSubhide() {
+    mobileLnbMenu.removeClass("mobile-show");
+    mobileSearchMenu.slideUp(300);
+  }
 });
